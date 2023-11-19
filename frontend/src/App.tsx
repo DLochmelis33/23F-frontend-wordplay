45108daa0c20436846cf57733aa8a2d8bf28527c
Denis Lochmelis <dlochmelis@gmail.com>
2023-11-19 18:53:56 +0100

    Basic game input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export const GameComponent: React.FC = () => {
+  console.log("ya")
+  const [userInput, setUserInput] = useState("uu")
+  const [gameLine, setGameLine] = useState("gl")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return (<>
+    <span id='currentLine'>{gameLine}</span>
+    <input
+      id='userInput'
+      value={userInput}
+      onChange={e => {
+        const newValue = e.target.value
+        const filtered = newValue.toLowerCase().replace(/[^a-z]/g, '') // drop all non-alphabetic characters
+        setUserInput(filtered)
+      }}
+      onKeyDown={e => {
+        if (e.key == 'Enter') {
+          setGameLine(gameLine + userInput)
+          setUserInput('')
+        }
+      }} />
+  </>);
 }
-
-export default App
